3341aa0e5d2b89392187ec58708873996c9f1b54
Evgeny <skosarew@mail.ru>
2019-07-10 11:18:28 +0000

    Task 1.9 for python3

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from urlparse import parse_qs
+from urllib.parse import parse_qs
 def application(environ, start_response):
     query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
     body = []
     for key, value in query.items():
         for item in value:
-            body.append(key + "=" + item + "\r\n")
+            body.append(bytes(key + "=" + item + "\r\n", 'utf-8'))
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
